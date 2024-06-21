c97be06255efa2d73a1a9456ea3754448b06b17c
Ehya-Ag <ehyaagmohamed23@gmail.com>
2024-06-21 09:59:24 -0500

    balise meta

--- script.js
+++ script.js
@@ -9,24 +9,24 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
 
-    const form = document.getElementById('contact-form');
-    form.addEventListener('submit', function(event) {
-        let isValid = true;
-        const inputs = form.querySelectorAll('input[required], textarea[required]');
-        inputs.forEach(input => {
-            if (!input.value.trim()) {
-                isValid = false;
-                input.classList.add('invalid');
-            } else {
-                input.classList.remove('invalid');
-            }
-        });
+    // const form = document.getElementById('contact-form');
+    // form.addEventListener('submit', function(event) {
+    //     let isValid = true;
+    //     const inputs = form.querySelectorAll('input[required], textarea[required]');
+    //     inputs.forEach(input => {
+    //         if (!input.value.trim()) {
+    //             isValid = false;
+    //             input.classList.add('invalid');
+    //         } else {
+    //             input.classList.remove('invalid');
+    //         }
+    //     });
 
-        if (!isValid) {
-            event.preventDefault();
-            alert('Veuillez remplir tous les champs obligatoires.');
-        }
-    });
+    //     if (!isValid) {
+    //         event.preventDefault();
+    //         alert('Veuillez remplir tous les champs obligatoires.');
+    //     }
+    // });
 
 
 });
